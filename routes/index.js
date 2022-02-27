673f5da94482a8c7195b4cc6b30c95377b640775
Connor Aalto <connoraalto@Connors-MBP-2.fios-router.home>
2022-02-27 16:26:07 -0500

    test

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,9 @@
 var express = require('express');
 var router = express.Router();
-let mysql = require('mysql');
 
-let connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'password',
-  database : 'sample_database',
-});
-
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  let sql = 'SELECT messages FROM sample_database.messages';
-  connection.query(sql, function(err, result) {
-  if (err) throw err;
-  res.json(result);
-
-})})
+  res.render('index', { title: 'Express' });
+});
 
 module.exports = router;
