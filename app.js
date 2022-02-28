5e3a04eb7a9c37567d057dbafd9025cf42c1d4a9
Connor Aalto <connoraalto@connors-mbp-2.andover.edu>
2022-02-28 08:51:03 -0500

    db and cors added

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var cors = require('cors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -18,6 +18,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+const corsOptions = {
+  origin: '*',
+  credentials:true,
+  optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
