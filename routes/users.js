8e3e1bd7bcac3e22b178166107de7728f2fe44d6
Connor Aalto <connoraalto@Connors-MBP-2.fios-router.home>
2022-02-27 21:13:32 -0500

    Updated with local database

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+let mysql = require('mysql');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+let connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'password',
+  database : 'sample_database',
 });
+
+router.get('/', function(req, res, next) {
+  let sql = 'SELECT messages FROM sample_database.messages';
+  connection.query(sql, function(err, result) {
+  if (err) throw err;
+  res.json(result);
+
+})})
 
 module.exports = router;
